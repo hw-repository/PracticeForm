774ae2b9e9fe1c3793c8e2d306f7ebcd7c991642
o.kucherenko <ku4erenkoElena@gmail>
2022-11-10 17:44:54 +0300

    add changes

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -61,74 +61,7 @@ public class FirstTest {
 
     }
 
-    @Test
-    void PracticeForm(){
 
-        HashMap<String,String> Student = new HashMap<String,String>();
-        Student.put("Name","Olena");
-        Student.put("Last Name","Kucherenko");
-        Student.put("Email","ku4erenkoElena@gmail.com");
-        Student.put("Gender","Female");
-        Student.put("Mobile","1234567890");
-        Student.put("Year of birth","1987");
-        Student.put("Month of birth","September");
-        Student.put("Day of birth","5");
-        Student.put("Subject","Computer Science");
-        Student.put("Hobbies","Reading");
-        Student.put("Picture","Kitten.jpg");
-        Student.put("Address","Minsk");
-        Student.put("State","Rajasthan");
-        Student.put("City","Jaipur");
-
-
-        open("/automation-practice-form");
-        executeJavaScript("$('footer').remove()");
-        executeJavaScript("$('#fixedban').remove()");
-        $("#firstName").setValue(Student.get("Name"));
-        $("#lastName").setValue(Student.get("Last Name"));
-        $("#userEmail").setValue(Student.get("Email"));
-        $("#genterWrapper").$(byText(Student.get("Gender"))).click();
-        $("#userNumber").setValue(Student.get("Mobile"));
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption(Student.get("Month of birth"));
-        $(".react-datepicker__year-select").selectOption(Student.get("Year of birth"));
-        //$("[aria-label='Choose Saturday, September 5th, 1987']").click();
-        $("[aria-label='Choose Saturday, "+Student.get("Month of birth")+" "+Student.get("Day of birth")+"th, "+Student.get("Year of birth")+"']").click();
-        $("#subjectsInput").sendKeys("C");
-        $(byText(Student.get("Subject"))).click();
-        $("#hobbiesWrapper").$(byText(Student.get("Hobbies"))).click();
-        File file = new File("src/test/resources/"+Student.get("Picture"));
-        $("#uploadPicture").uploadFile(file);
-        $("#currentAddress").setValue(Student.get("Address"));
-
-        //$("[id=react-select-4-input]").click();
-        $("#state").click();
-        $(byText(Student.get("State"))).click();
-        $("#city").click();
-        $(byText(Student.get("City"))).click();
-        $("#submit").click();
-
-
-        Student.forEach((k,v)->
-                $(".table-responsive").shouldHave(text(Student.get(k)))
-
-        );
-
-        $("#closeLargeModal").click();
-
-
-//
-
-
-
-
-
-
-
-
-
-
-    }
 
 
 }
